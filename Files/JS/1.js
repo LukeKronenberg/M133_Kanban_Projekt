1f2d72c90fde413546de3ecea8a2352a476c01b4
Virus7021 <kronenbergluke@gmail.com>
2020-12-06 12:55:58 +0100

    Added Add/Remove Card
    -User can now add a card to a column of his choice
    -User can delete a card
    -Minor file size optimisation

--- Files/JS/1.js
+++ Files/JS/1.js
@@ -35,17 +35,6 @@ class Card {
 
 var Cards = [];
 
-Cards.push(new Card(1, 1, "Yes", "Yes is Yes"));
-Cards.push(new Card(2, 2, "Yes", "No is Yes"));
-Cards.push(new Card(3, 3, "No", "Yes is No"));
-Cards.push(new Card(4, 3, "No", "No is No"));
-Cards.push(new Card(5, 2, "No", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque bibendum, erat vitae pretium gravida, magna arcu tincidunt risus, vel posuere quam velit quis odio. Morbi tristique tellus sit amet felis consectetur, vitae sagittis mauris bibendum. Maecenas tempor tempus dolor, non gravida urna. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Fusce tincidunt mi nulla, vel fermentum felis pharetra id. Nulla facilisi. Donec fermentum consequat interdum. Fusce maximus purus diam, nec iaculis mi auctor sit amet. Nunc gravida libero nec ante ultricies, eu egestas."));
-Cards.push(new Card(5, 2, "No", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque bibendum, erat vitae pretium gravida, magna arcu tincidunt risus, vel posuere quam velit quis odio. Morbi tristique tellus sit amet felis consectetur, vitae sagittis mauris bibendum. Maecenas tempor tempus dolor, non gravida urna. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Fusce tincidunt mi nulla, vel fermentum felis pharetra id. Nulla facilisi. Donec fermentum consequat interdum. Fusce maximus purus diam, nec iaculis mi auctor sit amet. Nunc gravida libero nec ante ultricies, eu egestas."));
-
-console.log(Cards);
-
-UpdateCards(Cards);
-
 function UpdateCards(array) {
     array.forEach(card => {
         if (card._Tab >= 1 && card._Tab <= 3) {
@@ -58,17 +47,33 @@ function UpdateCards(array) {
 
 function AddCard(card) {
     document.getElementById(`KanbanCards${card._Tab}`).innerHTML +=
-        `<div class="Kanban-Card" id="CardId${card._Id}">
+        `<div class="Kanban-Card box-bright" id="CardId${card._Id}">
+        <a onclick=DelCard(${card._Id})><img class="ImgDelCard" src="Images/trash.png" alt=""></a>
         <div class="Card-Title" id="Title">${card._Title}</div>
-        <img class="ImgDelCard" src="Images/trash.png" alt="">
         <div class="Card-Description" id="Description">${card._Description}</div>
     </div>`;
 }
 
-function ShowAddCardDialog(TabTmp) {
+function DelCard(cardId) {
+    document.getElementById(`CardId${cardId}`).remove();
+    Cards.forEach(async card =>{
+        if (card._Id == cardId) {
+            delete Cards[card._Id];
+        }
+    })
+}
 
+async function ToggleAddCardDialog(TabTmp) {
+    document.getElementById("Input-Column").value = TabTmp;
+    document.getElementById("AddCardDialog").classList.toggle("hidden");
 }
 
 function AddCardDialog() {
+    Cards.push(new Card(Cards.length, document.getElementById("Input-Column").value, document.getElementById("Input-Title").value, document.getElementById("Input-Description").value));
 
+    document.getElementById("Input-Title").value = "";
+    document.getElementById("Input-Description").value = "";
+
+    AddCard(Cards[Cards.length - 1]);
+    ToggleAddCardDialog();
 }
